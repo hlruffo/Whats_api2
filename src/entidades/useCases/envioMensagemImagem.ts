72f88a0a342dc4613055cf5dfcd2edec99a3b25a
hlruffo <hlruffo@gmail.com>
2022-11-18 11:05:47 -0300

    fim aula 3

--- src/entidades/useCases/envioMensagemImagem.ts
+++ src/entidades/useCases/envioMensagemImagem.ts
@@ -1,17 +1,17 @@
 import { Client, MessageMedia } from 'whatsapp-web.js';
-import { IEnvioMensagem } from '../interfaces/IEnvioMensagem';
+
 import { BaseEnvio } from './baseEnvio';
 
 
 export class EnvioMensagemImagem extends BaseEnvio {
-    constructor(private data: { para: string; mensagem: string ,path: string}) {
+    constructor(private data: { para: string; mensagem: string, path: string }) {
         super()
     }
 
     async enviar(): Promise<void> {
         const img = MessageMedia.fromFilePath(this.data.path)
 
-        await this.bot?.sendMessage(this.data.para, img, { caption: "Gostou da minha foto?" })
+        await this.bot?.sendMessage(this.data.para, img, { caption: this.data.mensagem })
         return;
 
     }
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,31 @@
+import { PATH_MEDIA } from './utils/constants';
 import { EnvioMensagemImagem } from './entidades/useCases/envioMensagemImagem';
 import { EnvioMensagemTexto } from './entidades/useCases/envioMensagemTexto';
 import { BotJS } from './entidades/botjs';
+import { EnvioMensagemAudio } from './entidades/useCases/envioMensagemAudio';
 
 const bot = new BotJS(1000)
 
 const mensagemTexto = new EnvioMensagemTexto({
-    para:"5521982222126@c.us",
+    para: "5521982222126@c.us",
     mensagem: "oi gatão",
 });
 const mensagemImagem = new EnvioMensagemImagem({
-    para:"5521982222126@c.us",
+    para: "5521982222126@c.us",
     mensagem: "curtiu minha pic?",
-    path: "src/media/Screenshot.png"
+    path: PATH_MEDIA + "Screenshot.png"
+});
+const mensagemAudio = new EnvioMensagemAudio({
+    para: "5521982222126@c.us",
+    mensagem: "curtiu minha pic?",
+    path: PATH_MEDIA + "audioteste.ogg",
+    audioAsVoice: false,
 });
 
-(async ()=>{
+(async () => {
     await bot.execute();
+    await bot.enviar(mensagemTexto)
     await bot.enviar(mensagemImagem)
+    await bot.enviar(mensagemAudio)
     
 })()
